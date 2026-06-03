91c9a96ab321f58ffa0025948584ae8253de92b3
Andres_sierra_linktic <andres.sierra@linktic.com>
2026-06-03 17:03:25 -0500

    Added coteditor macos

---
+++ macos/coteditor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title coteditor
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+open -n -a CotEditor
